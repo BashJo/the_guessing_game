c53568c02c6854b6301c710d95f948238ade7412
BashJo <94405079+BashJo@users.noreply.github.com>
2021-11-23 12:59:23 +0300

    added some code lines

--- guessinggame.sh
+++ guessinggame.sh
@@ -4,7 +4,8 @@
 function guess_files_num {
 	while true
 	do
-		echo "Guess how many files (without directories) are in the current directory! Type your guess down:"
+		echo "Guess how many files (without directories) are in the current directory!"
+		echo "Type your guess down:"
 		read guess
 		if [[ $guess -gt $(ls -l | egrep "^-" | wc -l) ]]
 		then 
@@ -13,10 +14,17 @@ function guess_files_num {
 		then
 			echo "Guess bigger!"
 		else 
-			echo "There is exactly $guess files in this directory! Good job!"
+			echo "There is exactly $guess files in this directory!"
+			echo "Good job! Congratulations!"
 			break
 		fi
 	done
 }
 
+echo "============================================"
+echo " Guessing Game is launched"
+echo "============================================"
 guess_files_num
+echo "============================================"
+echo "Game Over!"
+echo "============================================"
